c8892cd78bbb745703472071062c889a6e922f96
Maciej Ślusarz <maciej.slusarz@proton.me>
2026-01-10 13:39:46 +0100

    Add initial FastAPI files

--- api/app/main.py
+++ api/app/main.py
@@ -49,5 +49,21 @@ def generuj_i_zapisz_do_csv(nazwa_pliku="klienci_krakow.csv", liczba_osob=10):
         print(f"Wystąpił błąd podczas zapisu pliku: {e}")
 
 
-if __name__ == "__main__":
-    generuj_i_zapisz_do_csv()
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+#if __name__ == "__main__":
+#    generuj_i_zapisz_do_csv()
