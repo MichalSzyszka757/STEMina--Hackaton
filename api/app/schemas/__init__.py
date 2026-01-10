ea9fe9ec567fdd4dd7abb28758d9ff7c5e2c4e87
MaxSzarnicki <mszarnicki@student.agh.edu.pl>
2026-01-10 22:50:24 +0100

    refactor nazw

--- api/app/schemas/__init__.py
+++ api/app/schemas/__init__.py
@@ -1,8 +1,8 @@
 # Eksport schematów klienta
-from .create_client import ClientCreate, ClientResponse, ClientBase
+from .client import ClientCreate, ClientResponse, ClientBase
 
 # Eksport schematów dostawcy
-from .create_provider import ProviderCreate, ProviderResponse, ProviderBase
+from .provider import ProviderCreate, ProviderResponse, ProviderBase
 
 # Schematy taska
-from .create_task import CreateTask, TaskResponse
+from .task import CreateTask, TaskResponse
